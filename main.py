f595fdd3e599ccd68418438e319a9157f8c966a2
Oriol Tauleria <otauleria@sqli.com>
2018-10-22 20:51:37 +0200

    Reading  button

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from secrets import *
 from variables import *
 from telegram.ext import Updater
-from telegram.ext import CommandHandler
+from telegram.ext import CommandHandler, CallbackQueryHandler
 from telegram.ext import MessageHandler, Filters
+import telegram
+from telegram import bot
+from telegram import InlineKeyboardButton
+from telegram import InlineKeyboardMarkup
 
 updater = Updater(token=secret_token)
 
@@ -10,44 +14,48 @@ dispatcher = updater.dispatcher
 
 import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.INFO)
+  level=logging.INFO)
 
+
+def button(bot, update):
+  query = update.callback_query
+  switcher = {
+    "1": "afegeix-me" 
+  }
+  action = switcher.get(query.data,"error")
+
+  bot.edit_message_text(text="Action => : {}".format(action),
+    chat_id=query.message.chat_id,
+    message_id=query.message.message_id)
 
 def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text=welcome_text)
+  bot.send_message(chat_id=update.message.chat_id, text=welcome_text)
 
 def add_member(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text=add_member_text)
+  bot.send_message(chat_id=update.message.chat_id, text=add_member_text)
 
 def del_member(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text=del_member_text)
+  bot.send_message(chat_id=update.message.chat_id, text=del_member_text)
 
+
+def show_list(bot, update):
+  keyboard = [[InlineKeyboardButton("👍", callback_data='1'),
+    InlineKeyboardButton("⛔", callback_data='2')],
+    [InlineKeyboardButton("Treure algú altre ⛔", callback_data='3'),
+    InlineKeyboardButton("Afegir algú altre 👍 ", callback_data='4')]]
+  reply_markup = InlineKeyboardMarkup(keyboard)
+  update.message.reply_text('T\'hi afegeixes?', reply_markup=reply_markup)
 
 start_handler = CommandHandler('start', start)
 add_member_handler = CommandHandler('add_member', add_member)
 del_member_handler = CommandHandler('del_member', del_member)
+show_list_handler = CommandHandler('show_list', show_list)
 
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(add_member_handler)
 dispatcher.add_handler(del_member_handler)
-
-
-def echo(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
-echo_handler = MessageHandler(Filters.text, echo)
-dispatcher.add_handler(echo_handler)
-
-
-def build_menu(buttons,
-               n_cols,
-               header_buttons=None,
-               footer_buttons=None):
-    menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
-    if header_buttons:
-        menu.insert(0, header_buttons)
-    if footer_buttons:
-        menu.append(footer_buttons)
-    return menu
+dispatcher.add_handler(show_list_handler)
+dispatcher.add_handler(CallbackQueryHandler(button))
 
 
 updater.start_polling()
