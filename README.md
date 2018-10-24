dbb97696ddf6927d34b4d3d16d1d5193a63fc1d2
Oriol Tauleria <otauleria@sqli.com>
2018-10-24 17:15:18 +0200

    just saving

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # telegram-llista
 Bot per Telegram per fer llistes de persones
+pip install python-telegram-bot --upgrade
--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 from secrets import *
 from variables import *
-from telegram.ext import Updater
-from telegram.ext import CommandHandler, CallbackQueryHandler
-from telegram.ext import MessageHandler, Filters
+from telegram.ext import MessageHandler, Filters, Updater, CommandHandler, CallbackQueryHandler
 import telegram
-from telegram import bot
-from telegram import InlineKeyboardButton
-from telegram import InlineKeyboardMarkup
+import json
+from telegram import bot, InlineKeyboardButton, InlineKeyboardMarkup
 
 updater = Updater(token=secret_token)
-
 dispatcher = updater.dispatcher
 
 import logging
@@ -29,7 +25,12 @@ def button(bot, update):
     message_id=query.message.message_id)
 
 def start(bot, update):
-  bot.send_message(chat_id=update.message.chat_id, text=welcome_text)
+  with open('data.json') as origin:
+    data = json.load(origin)
+  origin.close
+  user_list = ''.join(data)
+  bot.send_message(chat_id=update.message.chat_id, text="welcome_text => {}".format(user_list))
+  
 
 def add_member(bot, update):
   bot.send_message(chat_id=update.message.chat_id, text=add_member_text)
@@ -38,24 +39,36 @@ def del_member(bot, update):
   bot.send_message(chat_id=update.message.chat_id, text=del_member_text)
 
 
-def show_list(bot, update):
+def show(bot, update):
   keyboard = [[InlineKeyboardButton("👍", callback_data='1'),
     InlineKeyboardButton("⛔", callback_data='2')],
     [InlineKeyboardButton("Treure algú altre ⛔", callback_data='3'),
     InlineKeyboardButton("Afegir algú altre 👍 ", callback_data='4')]]
   reply_markup = InlineKeyboardMarkup(keyboard)
-  update.message.reply_text('T\'hi afegeixes?', reply_markup=reply_markup)
+  user = update.message.frçom_user
+
+
+  
+  user_list = ['nothing']
+  user_list.append(user['username'])
+  user_list_test = ' '.join(user_list)
+  print("user list is {}".format(user_list_test))
+
+  with open('data.json', 'w') as outfile:
+      json.dump(user_list, outfile)
+  outfile.close
+  update.message.reply_text('T\'hi afegeixes USER_LIST: {} ---- {} ID: {} ?'.format(user_list, user['username'], user['id']), reply_markup=reply_markup)
+  
 
 start_handler = CommandHandler('start', start)
 add_member_handler = CommandHandler('add_member', add_member)
 del_member_handler = CommandHandler('del_member', del_member)
-show_list_handler = CommandHandler('show_list', show_list)
+show_handler = CommandHandler('show', show)
 
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(add_member_handler)
 dispatcher.add_handler(del_member_handler)
-dispatcher.add_handler(show_list_handler)
+dispatcher.add_handler(show_handler)
 dispatcher.add_handler(CallbackQueryHandler(button))
-
 
 updater.start_polling()
