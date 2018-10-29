5f387408f100771962a28aadf8c590a330af29bb
Oriol Tauleria <otauleria@sqli.com>
2018-10-29 08:50:49 +0100

    add and show members

--- People_list.py
+++ People_list.py
@@ -7,7 +7,7 @@ class People_list:
   def __init__(self, name, description):
     self._name = name
     self._description = description
-    self._accepted = []
+    self._accepted = [""]
 
   def get_name(self):
     return self._name
@@ -18,16 +18,18 @@ class People_list:
   def get_accepted(self):
     return self._accepted
 
-  def set_name(self, name):
-    self._name = name
-
-  def set_description(self, descriphttps://stackabuse.com/reading-and-writing-json-to-a-file-in-python/n):
-    self._description = descriptionhttps://stackabuse.com/reading-and-writing-json-to-a-file-in-python/
+  def set_description(self, description):
+        self._description = description
+  
+  def get_members(self):
+        return self._accepted
 
   def add_member(self,new_accepted):
     self._accepted.append(new_accepted)
     print("add_member")
 
+  def set_name(self, name):
+        self._name = name
 
 global lists 
 lists = []
@@ -38,10 +40,10 @@ def add_list(people_list):
 
 
 #Add user to list
-def user_add_list ():https://stackabuse.com/reading-and-writing-json-to-a-file-in-python/
-  name = input("""Enter the list https://stackabuse.com/reading-and-writing-json-to-a-file-in-python/name:
-   """)https://stackabuse.com/reading-and-writing-json-to-a-file-in-python/
-  description = input("Enter the https://stackabuse.com/reading-and-writing-json-to-a-file-in-python/list description: ")
+def user_add_list ():
+  name = input("""Enter the list name:
+     """)
+  description = input("Enter the description: ")
   newlist = People_list(name, description)
   add_list(newlist)
 
@@ -49,40 +51,60 @@ def user_add_list ():https://stackabuse.com/reading-and-writing-json-to-a-file-i
 #Print all lists
 def print_lists():
   print ("There are {} lists".format(len(lists)))
-  for list in lists:
-      show_list(list)
 
 
 # Print single list 
 def show_list(people_list):
   name = people_list.get_name()
   description = people_list.get_description()
-  accepted_list = people_list.get_accepted()
+  #accepted_list = people_list.get_accepted()
   print ("Name is {} and description {}".format(name,description))
 
 
 # Select list and print
-def select_list():
+def choose_list():
     #Check lists not empty 
     lists_names = [list.get_name() for list in lists]
     index = 1
-    print ("Select which list you wanna read: ")
+    print ("Available lists: ")
     for name in lists_names:
       print ("[{}]: {}".format(index,name))
       index += 1
-    list_id = int(input("Which list do you wanna read?")) - 1 
-    # Check  list_id < len is valid!
+    list_id = int(input("Select list: ")) - 1 
+    # Check  list_id < len and list_id <= len is valid!
+    return (list_id)
+
+
+# Select list and print
+def select_list():
+    list_id = choose_list()    # Check  list_id < len is valid!
     print ("Selected id is: {}".format(list_id))
     show_list(lists[list_id])
 
-def main():
 
+def show_members():
+      list_id = choose_list()
+      #show_list(lists[list_id])
+      members = lists[list_id].get_members()
+      print ("Members: ")
+      for member in members:
+        print ("{}".format(member))
+
+def add_member():
+      list_id = choose_list()
+      new_member = input("Enter the new member name: ")
+      lists[list_id].add_member(new_member)
+
+def main():
   while True: 
     message = """
 What you wanna do? Choose one:
 1: add list 
 2: show lists
-3: Select list  
+3: Select list
+4: add members  
+5: show members  
+
 -------------------------
 """
     option = input(message) 
@@ -90,6 +112,8 @@ What you wanna do? Choose one:
       '1': user_add_list,
       '2': print_lists,
       '3': select_list,
+      '4': add_member,
+      '5': show_members,
     }
     switch[option]()
 
