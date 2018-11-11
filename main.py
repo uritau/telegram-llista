05f6ed66cdd54981d1fb14dd4bcee83200ad8cf4
Oriol Tauleria <oriol.tauleria@gmail.com>
2018-11-11 18:55:48 +0100

    cc

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from telegram import bot
 import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
   level=logging.INFO)
-
+logger = logging.getLogger(__name__)
 
 user_list = []
 
@@ -19,23 +19,21 @@ def echo(bot, update):
   
 
 def add_member(bot, update):
-  user_to_add = re.sub('\+\s|\+','',update.message.text)
+  user_to_add = re.sub(r'\+\s|\+','',update.message.text)
   user_list.append(user_to_add)
   print ("{}".format("user to add: {}".format(user_to_add)))
-  update.message.reply_text("<b>{}</b> afegit a la llista!!!\n Apuntats: \n-{}".format(user_to_add,"\n-".join(user_list)),parse_mode=telegram.ParseMode.HTML)
+  update.message.reply_text("<b>{}</b> afegit a la llista!!!\n Apuntats: \n- {}".format(user_to_add,"\n- ".join(user_list)),parse_mode=telegram.ParseMode.HTML)
 
 def del_member(bot, update):
-  user_to_del = re.sub('\-\s|\-','',update.message.text)
+  user_to_del = re.sub(r'\-\s|\-','',update.message.text)
   print ("{}".format("user to del: {}".format(user_to_del)))
   user_list.remove(user_to_del)
-  update.message.reply_text("<b>{}</b> tret de la llista!! :(  \nApuntats: \n-{}".format(user_to_del,"\n- ".join(user_list)),parse_mode=telegram.ParseMode.HTML)
+  update.message.reply_text("<b>{}</b> tret de la llista!! :(  \nApuntats: \n- {}".format(user_to_del,"\n- ".join(user_list)),parse_mode=telegram.ParseMode.HTML)
 
 
 def show(bot, update):
   print("show: user-list: {}".format(user_list))
   update.message.reply_text("users in list: {}".format(user_list))
-
-
 
 def start(bot, update):
   with open('data.json') as origin:
@@ -46,6 +44,13 @@ def start(bot, update):
 
 def error(bot, update, error):
   logger.warning('Update "%s" caused error "%s"', update, error)
+
+
+def del_member(bot, update):
+  user_to_del = re.sub(r'\-\s|\-','',update.message.text)
+  print ("{}".format("user to del: {}".format(user_to_del)))
+  user_list.remove(user_to_del)
+  update.message.reply_text("<b>{}</b> tret de la llista!! :(  \nApuntats: \n- {}".format(user_to_del,"\n- ".join(user_list)),parse_mode=telegram.ParseMode.HTML)
 
 
 def main():
@@ -62,7 +67,9 @@ def main():
   dispatcher.add_handler(RegexHandler('^\-', del_member))
   dispatcher.add_handler(RegexHandler('^show', show))
   dispatcher.add_handler(CommandHandler('show', show))
-  dispatcher.add_handler(RegexHandler('^help', echo))
+  dispatcher.add_handler(RegexHandler('^help', help))
+  dispatcher.add_handler(CommandHandler('help', help))
+  
   updater.start_polling()
 
   updater.idle()
